722554c96cb08b1d033f987f2812461670e3886b
kaushlendrashukla <kaushlendrashukla2015@gmail.com>
2022-11-02 14:05:57 +0530

    task_11

--- task_13/main.js
+++ task_13/main.js
@@ -1,43 +1,6 @@
-//localStorage.setItem('name','shukla')
 
-//  ---------------------------------TASK 10
-// function savedata(){
-// var name = document.getElementById('name').value;
-// var email = document.getElementById('email').value;
-
-// localStorage.setItem("name",name)
-// localStorage.setItem("email",email)
-// };
-
-// ----------------------------------TASK 11
-
-// function savedata() {
-
-//     let data = {
-//         name: document.getElementById('name').value,
-//         email: document.getElementById('email').value
-//     }
-//     localStorage.setItem("users", JSON.stringify(data));
-
-// };
-
-// --------------------------------   TASK 12
-
-// function savedata() {
-//     var name = document.getElementById('name').value
-//     var email= document.getElementById('email').value
-//         let multiple_user = new Array();
-//         multiple_user=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[];
-//         multiple_user.push({
-//             "name": name,
-//             "email": email
-//         })
-//         localStorage.setItem(multiple_user.email , JSON.stringify(multiple_user));
-
-//     };
-var itemList = document.getElementById('items');
-let arr=JSON.parse(localStorage.getItem('formdata')) ||[];
-function savedata() {
+document.getElementById('my-form').addEventListener('submit',(e)=>{
+    e.preventDefault();
     var name = document.getElementById('name').value
     var email = document.getElementById('email').value;
    
@@ -45,18 +8,18 @@ function savedata() {
         name,
         email
     }
-    // data.push(obj)
-    // (localStorage.setItem('formdata', JSON.stringify(data)))
-    if (data.some((v) => { return v.email == email })) {
-        alert("already registered")
-    }
-    else{
-        arr.push(obj);
-        (localStorage.setItem('formdata', JSON.stringify(arr)))
-    }
-}
-let data = JSON.parse(localStorage.getItem('formdata')) || [];
-displaydata(data);
+   console.log(obj)
+    axios.post('https://crudcrud.com/api/402476c9b3e2451da05a7810ed0b41f8/appointmentsData',obj)
+    .then((response)=> {
+        displaydata([response.data]);
+        console.log(response)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+   
+})
+
 function displaydata(data){
  document.querySelector('.items').innerText=null;
 data.map((el,index) => {
@@ -72,11 +35,11 @@ data.map((el,index) => {
     del.addEventListener('click',()=>{
         data.splice(index,1)
         localStorage.setItem('formdata',JSON.stringify(data))
+      
         displaydata(data)
-    })
+    });
     new_add.append(span1, span2,del)
     document.querySelector('.items').append(new_add)
-
 })
 }
 
